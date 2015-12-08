cd6e676c1f0a0d4af1376d51223f654a30b95136
Shaden <shaden.pate.10@cnu.edu>
2015-12-08 17:47:38 -0500

    Initial coding

---
+++ DisplayPanel.java
@@ -0,0 +1,23 @@
+import java.awt.*;
+import java.net.URL;
+
+import javax.swing.*;
+
+public class DisplayPanel extends JPanel {
+	public DisplayPanel( ) {
+
+	JButton[] slots = new JButton[3]; //creates array of JButtons
+	Image question; //holds the ?
+	question = getImage("mystery.gif"); //puts the ? image into question using the method below
+	
+	for(int i=0; i<3; i++){  //creates new ImageIcon and puts all 3 jbuttons instead of text
+		slots[i] = new JButton(new ImageIcon(question));
+	}
+	}
+	
+private Image getImage(String filename) {
+	URL url = getClass().getResource( filename );
+	ImageIcon icon = new ImageIcon(url);
+	return icon.getImage();
+	}
+}
---
+++ SlotMachine.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+
+public class SlotMachine extends JFrame{
+
+	
+	
+	
+	
+	
+	
+	
+	  public static void main(String[] args) {
+      	DisplayPanel panel = new DisplayPanel();
+              panel.setVisible(true);
+      }
+	
+}
