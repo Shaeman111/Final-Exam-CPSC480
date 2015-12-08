a3a278217433e185236a46808c78ef2e8c2a243b
Shaden <shaden.pate.10@cnu.edu>
2015-12-08 18:18:04 -0500

    Visually Acceptable, no key listeners yet

--- DisplayPanel.java
+++ DisplayPanel.java
@@ -4,14 +4,15 @@ import java.net.URL;
 import javax.swing.*;
 
 public class DisplayPanel extends JPanel {
+	
 	public DisplayPanel( ) {
-
 	JButton[] slots = new JButton[3]; //creates array of JButtons
 	Image question; //holds the ?
 	question = getImage("mystery.gif"); //puts the ? image into question using the method below
 	
 	for(int i=0; i<3; i++){  //creates new ImageIcon and puts all 3 jbuttons instead of text
 		slots[i] = new JButton(new ImageIcon(question));
+	    add(slots[i]);
 	}
 	}
 	
--- SlotMachine.java
+++ SlotMachine.java
@@ -1,17 +1,31 @@
+import java.awt.BorderLayout;
+
 import javax.swing.*;
 
 public class SlotMachine extends JFrame{
 
+	  public SlotMachine() { 
+      	super("Slot Machine!");
+        setLayout(new BorderLayout());
+      	
+    final DisplayPanel slotPanel = new DisplayPanel();
+    this.add(slotPanel, BorderLayout.CENTER);
+	this.setSize(480,190);
 	
+	JPanel start_stop = new JPanel();
+	JButton sta_button = new JButton("Start");
+	JButton sto_button = new JButton("Stop");
+	start_stop.add(sta_button);
+	start_stop.add(sto_button);
 	
-	
-	
-	
-	
+	this.add(start_stop, BorderLayout.SOUTH);
+	 }
+
 	
 	  public static void main(String[] args) {
-      	DisplayPanel panel = new DisplayPanel();
+      	SlotMachine panel = new SlotMachine();
               panel.setVisible(true);
       }
 	
 }
+	  
